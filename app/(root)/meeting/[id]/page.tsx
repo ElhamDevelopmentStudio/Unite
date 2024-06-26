90d11a8a3bd03b9dae45e1ea86f689717f6cd099
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-06-27 00:25:07 +0430

    Modified the logic for setting up a meeting and parsing the token

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room: #{params.id}</div>;
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const { call, callLoading } = useGetById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || callLoading) return <Loader />;
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default Meeting;
